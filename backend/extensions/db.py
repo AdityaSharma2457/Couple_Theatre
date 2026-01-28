28265d84406f9aa634f10fb48a3603cd39c73ecb
Aditya Sharma <sharmadi.kota@gmail.com>
2026-01-28 12:29:48 +0530

    Update db.py

--- backend/extensions/db.py
+++ backend/extensions/db.py
@@ -1,8 +1,9 @@
+import os
 from flask_pymongo import PyMongo
 
 mongo = PyMongo()
 
 def init_db(app):
-    app.config["MONGO_URI"] = app.config.get("MONGO_URI")
-    print("🔥 USING MONGO URI:", app.config["MONGO_URI"])
+    app.config["MONGO_URI"] = os.getenv("MONGO_URI")
+    print("USING MONGO URI:", app.config["MONGO_URI"])
     mongo.init_app(app)
